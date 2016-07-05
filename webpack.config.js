67127a78acac92ef8c6cac065a699e8b50d7c963
selvagsz <selvaganeshbsg@gmail.com>
2016-07-06 00:10:02 +0530

    Basic setup

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/main.js'
+  },
+
+  output: {
+    path: './build',
+    filename: '[name].js'
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.js|jsx$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      inject: 'body'
+    })
+  ]
+}
